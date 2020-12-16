8282c0449d4ece9c93c1810be59446ea1d7456e4
Brian Rowe <brian@timescale.com>
2020-12-16 09:26:30 -0800

    Initial implementation of t-digest for postgres
    This change incoporates the rust t-digest implementation by Paul
    Meng and Maarten Deprez (https://github.com/MnO2/t-digest) and builds
    a postgres aggregate around it.  Note that the current version will
    not compile as it requires some modification to the t-digest library
    and will attempt to find the crate at a local path.

--- sql/aggregates.sql
+++ sql/aggregates.sql
@@ -1,8 +1,9 @@
-CREATE AGGREGATE approx_median(size int, value DOUBLE PRECISION)
+CREATE AGGREGATE t_digest(size int, value DOUBLE PRECISION)
 (
     sfunc=tdigest_trans,
     stype=internal,
     finalfunc=tdigest_final,
+    combinefunc=tdigest_combine,
     serialfunc = tdigest_serialize,
     deserialfunc = tdigest_deserialize
 )

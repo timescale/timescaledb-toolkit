f4d290cee4c884e95f3c9073b062d72301e6ef22
Brian Rowe <brian@timescale.com>
2021-06-01 08:40:25 -0700

    Enhance pg_type macro to handle enums

--- extension/src/time_series.rs
+++ extension/src/time_series.rs
@@ -12,34 +12,22 @@ use flat_serialize::*;
 pg_type! {
     #[derive(Debug)]
     struct TimeSeries {
-        #[flat_serialize::flatten]
-        series : SeriesType<'a>,
+        series: enum SeriesType {
+            type_id: u64,
+            SortedSeries: 1 {
+                num_points: u64,  // required to be aligned
+                points: [TSPoint; self.num_points],
+            },
+            NormalSeries: 2 {
+                start_ts: i64,
+                step_interval: i64,
+                num_vals: u64,  // required to be aligned
+                values: [f64; self.num_vals],
+            },
+        },
     }
 }
-
 json_inout_funcs!(TimeSeries);
-
-flat_serialize_macro::flat_serialize! {
-    #[derive(Debug)]
-    #[derive(serde::Serialize, serde::Deserialize)]
-    #[flat_serialize::field_attr(
-        fixed = r##"#[serde(deserialize_with = "crate::serialization::serde_reference_adaptor::deserialize")]"##,
-        variable = r##"#[serde(deserialize_with = "crate::serialization::serde_reference_adaptor::deserialize_slice")]"##,
-    )]
-    enum SeriesType {
-        type_id: u64,
-        SortedSeries: 1 {
-            num_points: u64,  // required to be aligned
-            points: [TSPoint; self.num_points],
-        },
-        NormalSeries: 2 {
-            start_ts: i64,
-            step_interval: i64,
-            num_vals: u64,  // required to be aligned
-            values: [f64; self.num_vals],
-        },
-    }
-}
 
 // hack to allow us to qualify names with "timescale_analytics_experimental"
 // so that pgx generates the correct SQL
@@ -52,7 +40,7 @@ impl<'input> TimeSeries<'input> {
     #[allow(dead_code)]
     pub fn to_internal_time_series(&self) -> InternalTimeSeries {
         match self.series {
-            SeriesType::SortedSeries{points, ..} => 
+            SeriesType::SortedSeries{points, ..} =>
                 InternalTimeSeries::Explicit(
                     ExplicitTimeSeries {
                         ordered: true,
@@ -65,12 +53,12 @@ impl<'input> TimeSeries<'input> {
                         start_ts: *start_ts,
                         step_interval: *step_interval,
                         values: values.to_vec(),
-                    }   
+                    }
                 ),
         }
     }
 
-    pub fn from_internal_time_series(series: InternalTimeSeries) -> TimeSeries<'input> {
+    pub fn from_internal_time_series(series: &InternalTimeSeries) -> TimeSeries<'input> {
         unsafe {
             match series {
                 InternalTimeSeries::Explicit(series) => {
@@ -108,7 +96,7 @@ pub fn unnest_series(
     series: timescale_analytics_experimental::TimeSeries,
 ) -> impl std::iter::Iterator<Item = (name!(time,pg_sys::TimestampTz),name!(value,f64))> + '_ {
     let iter: Box<dyn Iterator<Item=_>> = match series.series {
-        SeriesType::SortedSeries{points, ..} => 
+        SeriesType::SortedSeries{points, ..} =>
             Box::new(points.iter().map(|points| (points.ts, points.val))),
 
         SeriesType::NormalSeries{start_ts, step_interval, num_vals, values} =>

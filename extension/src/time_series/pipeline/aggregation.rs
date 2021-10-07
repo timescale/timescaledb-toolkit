c437987ec9f3526b630bf1e58affd9d08ad2a3ff
Joshua Lockerman <josh@timescale.com>
2021-10-07 14:21:50 -0400

    add `num_vals()` finalizer

--- extension/src/time_series/pipeline/aggregation.rs
+++ extension/src/time_series/pipeline/aggregation.rs
@@ -29,6 +29,7 @@ pub mod toolkit_experimental {
     varlena_type!(PipelineThenStatsAgg);
     varlena_type!(PipelineThenSum);
     varlena_type!(PipelineThenAverage);
+    varlena_type!(PipelineThenNumVals);
 }
 
 
@@ -317,6 +318,98 @@ extension_sql!(r#"
 ALTER FUNCTION "arrow_pipeline_then_average" SUPPORT toolkit_experimental.pipeline_average_support;
 "#);
 
+
+//
+// NUM_VALS
+//
+pg_type! {
+    #[derive(Debug)]
+    struct PipelineThenNumVals<'input> {
+        num_elements: u64,
+        elements: [Element; self.num_elements],
+    }
+}
+
+ron_inout_funcs!(PipelineThenNumVals);
+
+#[pg_extern(
+    immutable,
+    parallel_safe,
+    name="num_vals_cast",
+    schema="toolkit_experimental"
+)]
+pub fn num_vals_pipeline_element<'p, 'e>(
+    accessor: toolkit_experimental::AccessorNumVals<'p>,
+) -> toolkit_experimental::PipelineThenNumVals<'e> {
+    let _ = accessor;
+    build ! {
+        PipelineThenNumVals {
+            num_elements: 0,
+            elements: vec![].into(),
+        }
+    }
+}
+
+extension_sql!(r#"
+    CREATE CAST (toolkit_experimental.AccessorNumVals AS toolkit_experimental.PipelineThenNumVals)
+        WITH FUNCTION toolkit_experimental.num_vals_cast
+        AS IMPLICIT;
+"#);
+
+#[pg_operator(immutable, parallel_safe)]
+#[opname(->)]
+pub fn arrow_pipeline_then_num_vals<'s, 'p>(
+    timeseries: toolkit_experimental::TimeSeries<'s>,
+    pipeline: toolkit_experimental::PipelineThenNumVals<'p>,
+) -> i64 {
+    run_pipeline_elements(timeseries, pipeline.elements.iter())
+        .num_vals() as _
+}
+
+#[pg_operator(immutable, parallel_safe)]
+#[opname(->)]
+pub fn finalize_with_num_vals<'p, 'e>(
+    mut pipeline: toolkit_experimental::UnstableTimeseriesPipeline<'p>,
+    then_stats_agg: toolkit_experimental::PipelineThenNumVals<'e>,
+) -> toolkit_experimental::PipelineThenNumVals<'e> {
+    if then_stats_agg.num_elements == 0 {
+        // flatten immediately so we don't need a temporary allocation for elements
+        return unsafe {flatten! {
+            PipelineThenNumVals {
+                num_elements: pipeline.0.num_elements,
+                elements: pipeline.0.elements,
+            }
+        }}
+    }
+
+    let mut elements = replace(pipeline.elements.as_owned(), vec![]);
+    elements.extend(then_stats_agg.elements.iter());
+    build! {
+        PipelineThenNumVals {
+            num_elements: elements.len().try_into().unwrap(),
+            elements: elements.into(),
+        }
+    }
+}
+
+#[pg_extern(
+    immutable,
+    parallel_safe,
+    schema="toolkit_experimental"
+)]
+pub unsafe fn pipeline_num_vals_support(input: Internal)
+-> Internal {
+    pipeline_support_helper(input, |old_pipeline, new_element| unsafe {
+        let new_element = PipelineThenNumVals::from_datum(new_element, false, 0)
+            .unwrap();
+        finalize_with_num_vals(old_pipeline, new_element).into_datum().unwrap()
+    })
+}
+
+extension_sql!(r#"
+ALTER FUNCTION "arrow_pipeline_then_num_vals" SUPPORT toolkit_experimental.pipeline_num_vals_support;
+"#);
+
 #[cfg(any(test, feature = "pg_test"))]
 mod tests {
     use pgx::*;
@@ -510,6 +603,70 @@ mod tests {
                         Arithmetic(function:Abs,rhs:0),\
                         Arithmetic(function:Floor,rhs:0)\
                     ])'::pipelinethenaverage\
+                )");
+        });
+    }
+
+    #[pg_test]
+    fn test_num_vals_finalizer() {
+        Spi::execute(|client| {
+            client.select("SET timezone TO 'UTC'", None, None);
+            // using the search path trick for this test b/c the operator is
+            // difficult to spot otherwise.
+            let sp = client.select("SELECT format(' %s, toolkit_experimental',current_setting('search_path'))", None, None).first().get_one::<String>().unwrap();
+            client.select(&format!("SET LOCAL search_path TO {}", sp), None, None);
+            client.select("SET timescaledb_toolkit_acknowledge_auto_drop TO 'true'", None, None);
+
+            // we use a subselect to guarantee order
+            let create_series = "SELECT timeseries(time, value) as series FROM \
+                (VALUES ('2020-01-04 UTC'::TIMESTAMPTZ, 25.0), \
+                    ('2020-01-01 UTC'::TIMESTAMPTZ, 10.0), \
+                    ('2020-01-03 UTC'::TIMESTAMPTZ, 20.0), \
+                    ('2020-01-02 UTC'::TIMESTAMPTZ, 15.0), \
+                    ('2020-01-05 UTC'::TIMESTAMPTZ, 30.0)) as v(time, value)";
+
+            let val = client.select(
+                &format!("SELECT (series -> num_vals())::TEXT FROM ({}) s", create_series),
+                None,
+                None
+            )
+                .first()
+                .get_one::<String>();
+            assert_eq!(val.unwrap(), "5");
+        });
+    }
+
+    #[pg_test]
+    fn test_num_vals_pipeline_folding() {
+        Spi::execute(|client| {
+            client.select("SET timezone TO 'UTC'", None, None);
+            // using the search path trick for this test b/c the operator is
+            // difficult to spot otherwise.
+            let sp = client.select("SELECT format(' %s, toolkit_experimental',current_setting('search_path'))", None, None).first().get_one::<String>().unwrap();
+            client.select(&format!("SET LOCAL search_path TO {}", sp), None, None);
+            client.select("SET timescaledb_toolkit_acknowledge_auto_drop TO 'true'", None, None);
+
+            // `-> series()` should force materialization, but otherwise the
+            // pipeline-folding optimization should proceed
+            let output = client.select(
+                "EXPLAIN (verbose) SELECT \
+                timeseries('1930-04-05'::timestamptz, 123.0) \
+                -> ceil() -> abs() -> floor() \
+                -> num_vals();",
+                None,
+                None
+            ).skip(1)
+                .next().unwrap()
+                .by_ordinal(1).unwrap()
+                .value::<String>().unwrap();
+            assert_eq!(output.trim(), "Output: \
+                arrow_pipeline_then_num_vals(\
+                    timeseries('1930-04-05 00:00:00+00'::timestamp with time zone, '123'::double precision), \
+                    '(version:1,num_elements:3,elements:[\
+                        Arithmetic(function:Ceil,rhs:0),\
+                        Arithmetic(function:Abs,rhs:0),\
+                        Arithmetic(function:Floor,rhs:0)\
+                    ])'::pipelinethennumvals\
                 )");
         });
     }

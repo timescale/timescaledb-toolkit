bd62b27f0a8e7871de8de844aaa330afd641e764
Eric Gillespie <epg@timescale.com>
2022-11-08 12:46:20 -0600

    Add github action to automate the release process.
    This ties together all the work we've done so far, giving us that
    push-button release process.
    
    Some things are currently hard-coded off for quick and easy testing, but
    everything except triggering the package build is tested.

--- tools/dependencies.sh
+++ tools/dependencies.sh
@@ -9,6 +9,8 @@
 
 PG_VERSIONS='12 13 14'
 
+CARGO_EDIT=0.11.2
+
 # Keep synchronized with extension/Cargo.toml and `cargo install --version N.N.N cargo-pgx` in Readme.md .
 PGX_VERSION=0.5.4
 
@@ -19,3 +21,6 @@ RUST_COMPONENTS=clippy,rustfmt
 # We use fpm 1.14.2 to build RPMs.
 # TODO Use rpmbuild directly.
 FPM_VERSION=1.14.2
+
+GH_DEB_URL=https://cli.github.com/packages/pool/main/g/gh/gh_2.16.1_amd64.deb
+GH_DEB_SHA256=d0ba8693b6e4c1bde6683ccfa971a15c00b9fe92865074d48609959d04399dc7

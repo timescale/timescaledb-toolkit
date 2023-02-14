4390cea3301a4081d6a3acd0c6dc422ba730e5ec
Smitty <smitty@timescale.com>
2023-02-14 16:37:33 -0500

    Bump CI Rust version to 1.65

--- tools/dependencies.sh
+++ tools/dependencies.sh
@@ -16,7 +16,7 @@ CARGO_EDIT=0.11.2
 # Keep synchronized with extension/Cargo.toml and `cargo install --version N.N.N cargo-pgx` in Readme.md .
 PGX_VERSION=0.7.1
 
-RUST_TOOLCHAIN=1.64.0
+RUST_TOOLCHAIN=1.65.0
 RUST_PROFILE=minimal
 RUST_COMPONENTS=clippy,rustfmt
 

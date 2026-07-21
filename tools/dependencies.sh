2aeb7e8a0b3aa339db461f3cd4af0b9087e8a72d
Ivan Sanchez <surister98@gmail.com>
2026-07-21 19:54:11 +0200

    Update pgrx to 0.19.1

--- tools/dependencies.sh
+++ tools/dependencies.sh
@@ -14,7 +14,7 @@ TSDB_PG_VERSIONS='15 16 17 18'
 CARGO_EDIT=0.11.2
 
 # Keep synchronized with extension/Cargo.toml and `cargo install --version N.N.N cargo-pgrx` in Readme.md .
-PGRX_VERSION=0.18.1
+PGRX_VERSION=0.19.1
 
 RUST_TOOLCHAIN=1.96.0
 RUST_PROFILE=minimal
